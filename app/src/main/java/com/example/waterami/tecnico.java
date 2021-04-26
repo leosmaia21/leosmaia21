d0f0ddb85bf0dec6a058ba9dd960ef6479072fd6
leosmaia21 <77647786+leosmaia21@users.noreply.github.com>
2021-04-26 15:08:04 +0100

    libraria graficos

---
+++ app/src/main/java/com/example/waterami/tecnico.java
@@ -0,0 +1,14 @@
+package com.example.waterami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tecnico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tecnico);
+    }
+}
